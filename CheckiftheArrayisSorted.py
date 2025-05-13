9c5fd13ecaff4cb087f0d3ae457500b2c8c82f6d
Satya Parsi <satyaparsi2000@gmail.com>
2025-05-13 14:10:46 -0500

    Day 3 changes

--- CheckiftheArrayisSorted.py
+++ CheckiftheArrayisSorted.py
@@ -37,6 +37,19 @@
 
 # Time limit: 1 sec
 
+
+# Optimal Solution
+# 1. Problem Statement
+
+# Our goal is to determine whether a given array of integers is sorted in non-decreasing order and then possibly rotated. The function checks whether there is at most one "rotation point" in the array, which would indicate that the array is sorted and rotated.
+# Input:
+# nums: A list of integers.
+# Output:
+# True if the array is sorted and rotated; otherwise, False.
+# 2. Intuition and Approach
+# The problem involves verifying if an array that was originally sorted in non-decreasing order has been rotated. A rotation means that a prefix of the array has been moved to the end. For example, the sorted array [1, 2, 3, 4, 5] could be rotated to become [4, 5, 1, 2, 3].
+# The key observation is that in a rotated and sorted array, there should be exactly one place where a "decrease" occurs (i.e., where an element is greater than the next element). This drop signifies the rotation point. If there is more than one such drop, the array is neither sorted nor correctly rotated.
+
 class Solution:
     def check(self, nums: List[int]) -> bool:
         n = len(nums)
