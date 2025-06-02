ebda6a5a6fc5cd209a4d338d2998b00abb6fb34e
Satya Parsi <satyaparsi2000@gmail.com>
2025-06-01 22:47:21 -0500

    Valid Anagram

---
+++ Strings/Easy/242.ValidAnagram.py
@@ -0,0 +1,33 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+
+# Output: true
+
+# Example 2:
+
+# Input: s = "rat", t = "car"
+
+# Output: false
+
+ 
+
+# Constraints:
+
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_c=s
+        t_c=t
+        sort_s=sorted(s)
+        sort_t=sorted(t)
+        if sort_s==sort_t:
+            return True
+        else:
+            return False
